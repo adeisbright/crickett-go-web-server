7ada2cfa2665ddf057955b442558bc277b4c4dc1
Adeleke Bright <bigjaratechnologies@gmail.com>
2024-03-13 06:00:24 +0100

    Step 1 : Add Error Handling on Startup Error

--- main.go
+++ main.go
@@ -13,9 +13,11 @@ func HandleRequest(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fmt.Println("Starting The Web Server on port 8000")
 	http.HandleFunc("/", HandleRequest)
-	server := http.Server{
+	server := &http.Server{
 		Addr: "localhost:8000",
 	}
 
-	server.ListenAndServe()
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		fmt.Println("Error with Starting Server", err)
+	}
 }
