26d21bb5ac3ab000d95ba709f59b60e711d0400d
Adeleke Bright <bigjaratechnologies@gmail.com>
2024-03-13 06:19:45 +0100

    Step 1 :Handle a Single Request

--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
+)
+
+var (
+	mutex sync.Mutex
 )
 
 func HandleRequest(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
 	path := r.URL.Path
 	fmt.Fprintf(w, "%s", path)
 }
